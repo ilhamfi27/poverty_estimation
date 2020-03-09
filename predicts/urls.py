41ebb30f7554c8193d530a78fcb8d708f327d91c
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2020-03-10 06:33:18 +0700

    complete the design

--- predicts/urls.py
+++ predicts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('dataset_insert/', views.dataset_insert, name='dataset_insert'),
+    path('dataset_list/', views.dataset_list, name='dataset_list'),
+    path('predictor/', views.predictor, name='predictor'),
+    path('predict_mapping/', views.predict_mapping, name='predict_mapping'),
 ]
--- predicts/views.py
+++ predicts/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
+
 def dataset_insert(request):
     return render(request, 'dataset_insert.html', {})
+
+
+def dataset_list(request):
+    return render(request, 'dataset_list.html', {})
+
+
+def predictor(request):
+    return render(request, 'predictor.html', {})
+
+
+def predict_mapping(request):
+    return render(request, 'predict_mapping.html', {})
