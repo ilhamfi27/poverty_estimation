4b4bea1d0efe794d4c4b35d8fec57f4770662031
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2020-03-11 14:23:34 +0700

    developing insert dataset

--- datasets/views.py
+++ datasets/views.py
@@ -1,8 +1,65 @@
 from django.shortcuts import render
+from .models import Dataset
+import pandas as pd
+
+dataset_column_names = [
+    'city_id', 'BPS_poverty_rate', 'sum_price_car', 'avg_price_car', 'std_price_car', 'sum_sold_car',
+    'avg_sold_car', 'std_sold_car', 'sum_viewer_car', 'avg_viewer_car', 'std_viewer_car', 'sum_buyer_car',
+    'avg_buyer_car', 'std_buyer_car', 'sum_price_motor', 'avg_price_motor', 'std_price_motor', 'sum_sold_motor',
+    'avg_sold_motor', 'std_sold_motor', 'sum_viewer_motor', 'avg_viewer_motor', 'std_viewer_motor',
+    'sum_buyer_motor', 'avg_buyer_motor', 'std_buyer_motor', 'sum_price_rumah_sell', 'avg_price_rumah_sell',
+    'std_price_rumah_sell', 'sum_sold_rumah_sell', 'avg_sold_rumah_sell', 'std_sold_rumah_sell',
+    'sum_viewer_rumah_sell', 'avg_viewer_rumah_sell', 'std_viewer_rumah_sell', 'sum_buyer_rumah_sell',
+    'avg_buyer_rumah_sell', 'std_buyer_rumah_sell', 'sum_price_rumah_rent', 'avg_price_rumah_rent',
+    'std_price_rumah_rent', 'sum_sold_rumah_rent', 'avg_sold_rumah_rent', 'std_sold_rumah_rent',
+    'sum_viewer_rumah_rent', 'avg_viewer_rumah_rent', 'std_viewer_rumah_rent', 'sum_buyer_rumah_rent',
+    'avg_buyer_rumah_rent', 'std_buyer_rumah_rent', 'sum_price_apt_sell', 'avg_price_apt_sell',
+    'std_price_apt_sell', 'sum_sold_apt_sell', 'avg_sold_apt_sell', 'std_sold_apt_sell', 'sum_viewer_apt_sell',
+    'avg_viewer_apt_sell', 'std_viewer_apt_sell', 'sum_buyer_apt_sell', 'avg_buyer_apt_sell', 'std_buyer_apt_sell',
+    'sum_price_apt_rent', 'avg_price_apt_rent', 'std_price_apt_rent', 'sum_sold_apt_rent', 'avg_sold_apt_rent',
+    'std_sold_apt_rent', 'sum_viewer_apt_rent', 'avg_viewer_apt_rent', 'std_viewer_apt_rent', 'sum_buyer_apt_rent',
+    'avg_buyer_apt_rent', 'std_buyer_apt_rent', 'sum_price_land_sell', 'avg_price_land_sell', 'std_price_land_sell',
+    'sum_sold_land_sell', 'avg_sold_land_sell', 'std_sold_land_sell', 'sum_viewer_land_sell',
+    'avg_viewer_land_sell', 'std_viewer_land_sell', 'sum_buyer_land_sell', 'avg_buyer_land_sell',
+    'std_buyer_land_sell', 'sum_price_land_rent', 'avg_price_land_rent', 'std_price_land_rent',
+    'sum_sold_land_rent', 'avg_sold_land_rent', 'std_sold_land_rent', 'sum_viewer_land_rent',
+    'avg_viewer_land_rent', 'std_viewer_land_rent', 'sum_buyer_land_rent', 'avg_buyer_land_rent',
+    'std_buyer_land_rent'
+]
+
 
 def new(request):
-    return render(request, 'new.html', {})
+    if request.method == 'GET':
+        return render(request, 'new.html', {})
+    else:
+        source_file = request.FILES['dataset_source']
+        file_extension = repr(str(source_file).split('.')[-1])
+
+        dataframe = pd.read_excel(source_file)
+
+        source_data = []
+        for row in dataframe.iloc[0:, :].values:
+            row_data = []
+            for cell in row:
+                row_data.append(cell)
+            zipped_data = dict(zip(dataset_column_names, row_data))
+
+            # make city id integer instead of float
+            zipped_data['city_id'] = int(zipped_data['city_id'])
+
+            dataset_insert(zipped_data)
+
+        return render(request, 'new.html', {})
 
 
 def list(request):
-    return render(request, 'list.html', {})
+    table_header = dataset_column_names
+    table_data = Dataset.objects.all()
+
+    print(table_data, flush=True)
+
+    return render(request, 'list.html', {'table_header': table_header})
+
+
+def dataset_insert(data):
+    return Dataset.objects.create(**data)
